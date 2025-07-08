1b42470ba21bcfa03810f273c2c31d771da848a6
Aleydon <aleydonroberto@gmail.com>
2025-07-08 06:36:03 -0300

    chore(initial commit): setup project

---
+++ src/pages/create-room.tsx
@@ -0,0 +1,9 @@
+export function CreateRoom() {
+	return (
+		<div className="h-screen w-full">
+			<a className="text-white underline" href="/room">
+				Acessar sala
+			</a>
+		</div>
+	);
+}
---
+++ src/pages/room.tsx
@@ -0,0 +1,7 @@
+export function Room() {
+	return (
+		<div>
+			<h3>Room Details</h3>
+		</div>
+	);
+}
